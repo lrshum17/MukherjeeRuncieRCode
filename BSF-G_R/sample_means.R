cd6a91974dc9bdf02a98713655974ca47af4fde4
Wenli Shi <gray.wenli.shi@gmail.com>
2016-04-18 14:59:20 -0400

    Finish sample_means

--- BSF-G_R/sample_means.R
+++ BSF-G_R/sample_means.R
@@ -1,6 +1,17 @@
 sample_means = function(Ytil, Qt_Design, N, ..., resid, random_precision, svd_Design_Ainv){
+  # %when used to sample [B;D]:
+  #   %Y - FL' - Z_2W = XB + ZD + E, vec(E)~N(0,kron(Psi_E,In)). 
+  # % Note: conditioning on F, L and W.
+  # %The vector [b_j;d_j] is sampled simultaneously. Each trait is sampled separately because their
+  # %conditional posteriors factor into independent MVNs.
+  # %note:svd_Design_Ainv has parameters to diagonalize mixed model equations for fast inversion: 
+  # %inv(a*blkdiag(fixed_effects.cov,Ainv) + b*[X; Z_1][X; Z_1]') = Q*diag(1./(a.*s1+b.*s2))*Q'
+  # %Qt_Design = Q'*Design, which doesn't change each iteration. Design = [X;Z_1]
+  # %
+  # %function also used to sample W:
+  # %Y - FL' - XB - ZD = Z_2W + E, vec(E)~N(0,kron(Psi_E,In)). 
+  # %Here, conditioning is on B and D.
   p=resid$p
-  
   Q = svd_Design_Ainv$Q
   s1 = svd_Design_Ainv$s1
   s2 = svd_Design_Ainv$s2
