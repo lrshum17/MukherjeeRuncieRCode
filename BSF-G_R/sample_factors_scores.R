0e6d421099e3a60de72ee579cb151dd4ade017f7
Leo Shu <lrshum17@outlook.com>
2016-04-30 19:38:23 -0400

    sample_factors r rmd

--- BSF-G_R/sample_factors_scores.R
+++ BSF-G_R/sample_factors_scores.R
@@ -1,4 +1,4 @@
-#Need to define Z_1 before using
+#Need to define Z_1 before using. NEED TO USE inv()!?
 
 sample_factors_scores = function(Ytil, Factors, resid, genetic_effects, Z_1){
   #Sample factor scores given factor loadings, U, factor heritabilities and
@@ -13,18 +13,3 @@ sample_factors_scores = function(Ytil, Factors, resid, genetic_effects, Z_1){
   Factors$scores = Meta + inv(t(S)) %*% matrix(rnorm(k*n), nrow = k) 
   return(Factors)
 }
-
-# function [ Factors ] = sample_factors_scores( Ytil, Factors,resid,genetic_effects )
-# %Sample factor scores given factor loadings, U, factor heritabilities and
-# %phenotype residuals
-# global Z_1
-# k = Factors.k;
-# n = Factors.n;
-# Lambda = Factors.Lambda;
-# Lmsg = bsxfun(@times,Lambda,resid.ps);
-# tau_e = 1./(1-Factors.h2);
-# S=chol(Lambda'*Lmsg+diag(tau_e),'lower');
-# Meta = S'\(S\(Lmsg'*Ytil + bsxfun(@times,genetic_effects.U*Z_1,tau_e)));
-# Factors.scores = Meta + S'\randn(k,n);   
-# end
-#               
